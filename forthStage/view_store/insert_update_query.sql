f5d126f7c39e3772adaa3fb3cdcf4e35085f61a7
Tehila Benezra <tehila151as@gmail.com>
2024-07-23 18:07:04 +0300

    forthStage

--- forthStage/view_store/insert_update_query.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Client (Client_Id, Client_Name, Is_Club_Member)
-VALUES (876, 'michal Doe', 1);
-
-UPDATE Client
-SET Client_Name = 'michal Doe', Is_Club_Member = 1
-WHERE Client_Id = 876;
--- forthStage/view_store/select_check1.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  Worker_Id,
-  Worker_Name,
-  Order_Id,
-  Order_Date
-FROM 
-  ClientPurchaseWorkerOrder
-WHERE 
-  Worker_Id IS NOT NULL;
